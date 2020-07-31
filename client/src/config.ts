b289954e8fb9ea9eb97833b91eed15b62b489714
Sonvi Kharbanda <kharbanda.sonvi@gmail.com>
2020-07-31 11:02:26 -0400

    updated files

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'kharbanda.sonvi.auth0.com',            // Auth0 domain
-  clientId: 'j5YpiIoJGsFtgU2A20Ey9Db6K9CgssPR',          // Auth0 client id
+  domain: 'dev-t-p4ns0j.us.auth0.com',            // Auth0 domain
+  clientId: '9M8mm1II2Hj1vjWV2H7js9L8BTbHl4h3',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
